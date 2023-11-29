a252d3d508d28828aa435cf3c73c4c1fd39262ed
Awadesh <36awadesh@gmail.com>
2023-11-29 22:27:44 +0530

    added pages, Product list , navbar

--- src/features/product-list/ProductListSlice.js
+++ src/features/product-list/ProductListSlice.js
@@ -1,11 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { fetchCount } from './productListAPI';
 
 const initialState = {
   value: 0,
   status: 'idle',
 };
-
 
 export const incrementAsync = createAsyncThunk(
   'counter/fetchCount',
@@ -23,9 +22,7 @@ export const counterSlice = createSlice({
     increment: (state) => {
       state.value += 1;
     },
-
   },
-
   extraReducers: (builder) => {
     builder
       .addCase(incrementAsync.pending, (state) => {
@@ -41,6 +38,5 @@ export const counterSlice = createSlice({
 export const { increment } = counterSlice.actions;
 
 export const selectCount = (state) => state.counter.value;
-
 
 export default counterSlice.reducer;
